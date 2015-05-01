eafafae33f91ce7291ab0bf294fa4a6e5ff2655d
Bernardo Casaleiro <bernardocasaleiro@gmail.com>
2015-05-01 20:34:28 +0100

    some changes

--- src/projecto/Simulation.java
+++ src/projecto/Simulation.java
@@ -24,26 +24,16 @@ public class Simulation {
         ioStation = new Process(null, "Simple Process-Oriented Van Carrier Model", true, true, getBackToIO);
         ioStation.connectToExperiment(exp);
         
-        // set experiment parameters
-        exp.setShowProgressBar(true);  // display a progress bar (or not)
+        exp.setShowProgressBar(true);  
         exp.stop(new TimeInstant(runtime, TimeUnit.MINUTES));
         exp.tracePeriod(new TimeInstant(0), new TimeInstant(runtime, TimeUnit.MINUTES));
-        // set the period of the trace
+        
         exp.debugPeriod(new TimeInstant(0), new TimeInstant(runtime, TimeUnit.MINUTES));   // and debug output
-        // ATTENTION!
-        // Don't use too long periods. Otherwise a huge HTML page will
-        // be created which crashes Netscape :-)    
-        // start the experiment at simulation time 0.0
+        
         exp.start();
 
-        // --> now the simulation is running until it reaches its end criterion
-        // ...
-        // ...
-        // <-- afterwards, the main thread returns here
-        // generate the report (and other output files)
         exp.report();
 
-        // stop all threads still alive and close all output files
         exp.finish();
     }
 }
